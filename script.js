1b7b32906d235a6e195f529e3b9fac00f1e7fbaf
Fredgomes28 <d2023006205@unifei.edu.br>
2024-04-25 00:38:16 -0300

    9

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Função para fazer a rolagem dos botões//
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
